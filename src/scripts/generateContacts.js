b53d3be82e5a0f9141a642757a103a1ae5f1a78f
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-09-07 15:08:04 +0300

    update project

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,22 +1,3 @@
-import { promises as fs } from 'fs';
-import { createFakeContact } from '../utils/createFakeContact.js';
-import { PATH_DB } from '../constants/contacts.js';
-
-const generateContacts = async (number) => {
-  try {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
-    const contacts = JSON.parse(data);
-
-    for (let i = 0; i < number; i++) {
-      const newContact = createFakeContact();
-      contacts.push(newContact);
-    }
-
-    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
-    console.log(`${number}`);
-  } catch (error) {
-    console.error(error);
-  }
-};
+const generateContacts = async (number) => {};
 
 generateContacts(5);
